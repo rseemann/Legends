58f0b729c0bea0419114f8998b9b985d199aeb3f
Rafael <rseemann@gmail.com>
2010-05-18 18:20:06 -0300

    Adding modifications that will enable us saving the character.

--- src/SheetGUI/Sheet.java
+++ src/SheetGUI/Sheet.java
@@ -6,15 +6,20 @@ import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import ActionListeners.SaveMenuItemListener;
 import Ficha.RPGCharacter;
-import RoladorDeDadosL5RGUI.Display;
 
 public class Sheet {
 	private JFrame mainWindow;
@@ -26,14 +31,31 @@ public class Sheet {
 
 	public void addMainWindowItems(Container pane) {
 
-		// -----trait panel-----
-		topPanel = new JPanel();
-		topPanel.setBackground(Color.white);
-		
+		// -----top panel-----
+		topPanel = new JPanel(new BorderLayout());
+		topPanel.setBackground(Color.lightGray);
+
+		// ---menu----
+
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		JMenuItem newMenuItem = new JMenuItem("Edit");
+		JMenuItem saveMenuItem = new JMenuItem("Save");
+		saveMenuItem.addActionListener(new SaveMenuItemListener());
+
+		fileMenu.add(newMenuItem);
+		fileMenu.add(saveMenuItem);
+		menuBar.add(fileMenu);
+		mainWindow.setJMenuBar(menuBar);
+
+		// -------Character Info Panel--------
+
 		JPanel topPanelInfo = new JPanel(new GridBagLayout());
 		GridBagConstraints tpi = new GridBagConstraints();
-		topPanelInfo.setBackground(Color.red);
-		
+		topPanelInfo.setBackground(Color.lightGray);
+
+		tpi.insets = new Insets(10, 0, 0, 0);
+
 		JLabel nameLabel = new JLabel("Name: ");
 		JTextField nameField = new JTextField(16);
 
@@ -81,21 +103,219 @@ public class Sheet {
 		tpi.gridx = 9;
 		topPanelInfo.add(insightField, tpi);
 
-		//----rings jpegs----
+		// ----Character Traits and Rings Panel----
 		JPanel topPanelRings = new JPanel(new GridBagLayout());
 		GridBagConstraints tpr = new GridBagConstraints();
-		topPanelRings.setBackground(Color.green);
-		
+		topPanelRings.setBackground(Color.lightGray);
+
 		String imagesPath = "resources/img/images/pics/";
-		JLabel earthRingJPEG = new JLabel(new ImageIcon(imagesPath+"RingOfEarth.jpg"));
-		
+		JLabel earthRingJPEG = new JLabel(new ImageIcon(imagesPath
+				+ "RingOfEarth.jpg"));
+		JLabel waterRingJPEG = new JLabel(new ImageIcon(imagesPath
+				+ "RingOfWater.jpg"));
+		JLabel fireRingJPEG = new JLabel(new ImageIcon(imagesPath
+				+ "RingOfFire.jpg"));
+		JLabel airRingJPEG = new JLabel(new ImageIcon(imagesPath
+				+ "RingOfAir.jpg"));
+		JLabel voidRingJPEG = new JLabel(new ImageIcon(imagesPath
+				+ "RingOftheVoid.jpg"));
+
+		// earth ring
+		tpr.insets = new Insets(10, 0, 0, 0);
 		tpr.ipadx = 10;
 		tpr.gridx = 0;
 		tpr.gridy = 0;
-		//topPanelRings.add(earthRingJPEG, tpr);
-		
-		
-		//addings panels to topPanel
+		tpr.gridwidth = 2;
+		topPanelRings.add(earthRingJPEG, tpr);
+		tpr.insets = new Insets(3, 0, 0, 0);
+
+		JLabel earthRingLabel = new JLabel("Earth: ");
+		JTextField earthRingField = new JTextField(3);
+		JLabel staminaLabel = new JLabel("Stamina: ");
+		JTextField staminaField = new JTextField(3);
+		JLabel willpowerLabel = new JLabel("Willpower: ");
+		JTextField willpowerField = new JTextField(3);
+		tpr.gridwidth = 1;
+
+		tpr.gridy = 1;
+		tpr.gridx = 0;
+		topPanelRings.add(earthRingLabel, tpr);
+
+		tpr.gridy = 1;
+		tpr.gridx = 1;
+		topPanelRings.add(earthRingField, tpr);
+
+		tpr.gridy = 2;
+		tpr.gridx = 0;
+		topPanelRings.add(staminaLabel, tpr);
+
+		tpr.gridy = 2;
+		tpr.gridx = 1;
+		topPanelRings.add(staminaField, tpr);
+
+		tpr.gridy = 3;
+		tpr.gridx = 0;
+		topPanelRings.add(willpowerLabel, tpr);
+
+		tpr.gridy = 3;
+		tpr.gridx = 1;
+		topPanelRings.add(willpowerField, tpr);
+
+		// water Ring
+		tpr.insets = new Insets(10, 0, 0, 0);
+		tpr.gridy = 0;
+		tpr.gridx = 2;
+		tpr.gridwidth = 2;
+		topPanelRings.add(waterRingJPEG, tpr);
+		tpr.insets = new Insets(3, 0, 0, 0);
+
+		JLabel waterRingLabel = new JLabel("Water: ");
+		JTextField waterRingField = new JTextField(3);
+		JLabel strengthLabel = new JLabel("Strength: ");
+		JTextField strengthField = new JTextField(3);
+		JLabel perceptionLabel = new JLabel("Perception: ");
+		JTextField perceptionField = new JTextField(3);
+
+		tpr.gridwidth = 1;
+		tpr.gridy = 1;
+		tpr.gridx = 2;
+		topPanelRings.add(waterRingLabel, tpr);
+
+		tpr.gridy = 1;
+		tpr.gridx = 3;
+		topPanelRings.add(waterRingField, tpr);
+
+		tpr.gridy = 2;
+		tpr.gridx = 2;
+		topPanelRings.add(strengthLabel, tpr);
+
+		tpr.gridy = 2;
+		tpr.gridx = 3;
+		topPanelRings.add(strengthField, tpr);
+
+		tpr.gridy = 3;
+		tpr.gridx = 2;
+		topPanelRings.add(perceptionLabel, tpr);
+
+		tpr.gridy = 3;
+		tpr.gridx = 3;
+		topPanelRings.add(perceptionField, tpr);
+
+		// fire ring
+		tpr.insets = new Insets(10, 0, 0, 0);
+		tpr.gridy = 0;
+		tpr.gridx = 4;
+		tpr.gridwidth = 2;
+		topPanelRings.add(fireRingJPEG, tpr);
+		tpr.insets = new Insets(3, 0, 0, 0);
+
+		tpr.gridwidth = 1;
+		JLabel fireRingLabel = new JLabel("Fire: ");
+		JTextField fireRingField = new JTextField(3);
+		JLabel agilityLabel = new JLabel("Agility: ");
+		JTextField agilityField = new JTextField(3);
+		JLabel intelligenceLabel = new JLabel("Intelligence: ");
+		JTextField intelligenceField = new JTextField(3);
+
+		tpr.gridwidth = 1;
+		tpr.gridy = 1;
+		tpr.gridx = 4;
+		topPanelRings.add(fireRingLabel, tpr);
+
+		tpr.gridy = 1;
+		tpr.gridx = 5;
+		topPanelRings.add(fireRingField, tpr);
+
+		tpr.gridy = 2;
+		tpr.gridx = 4;
+		topPanelRings.add(agilityLabel, tpr);
+
+		tpr.gridy = 2;
+		tpr.gridx = 5;
+		topPanelRings.add(agilityField, tpr);
+
+		tpr.gridy = 3;
+		tpr.gridx = 4;
+		topPanelRings.add(intelligenceLabel, tpr);
+
+		tpr.gridy = 3;
+		tpr.gridx = 5;
+		topPanelRings.add(intelligenceField, tpr);
+
+		// air ring
+		tpr.insets = new Insets(10, 0, 0, 0);
+		tpr.gridy = 0;
+		tpr.gridx = 6;
+		tpr.gridwidth = 2;
+		topPanelRings.add(airRingJPEG, tpr);
+		tpr.insets = new Insets(3, 0, 0, 0);
+		tpr.gridwidth = 1;
+
+		JLabel airRingLabel = new JLabel("Air: ");
+		JTextField airRingField = new JTextField(3);
+		JLabel reflexesLabel = new JLabel("Reflexes: ");
+		JTextField reflexesField = new JTextField(3);
+		JLabel awarenessLabel = new JLabel("Awareness: ");
+		JTextField awarenessField = new JTextField(3);
+
+		tpr.gridwidth = 1;
+		tpr.gridy = 1;
+		tpr.gridx = 6;
+		topPanelRings.add(airRingLabel, tpr);
+
+		tpr.gridy = 1;
+		tpr.gridx = 7;
+		topPanelRings.add(airRingField, tpr);
+
+		tpr.gridy = 2;
+		tpr.gridx = 6;
+		topPanelRings.add(reflexesLabel, tpr);
+
+		tpr.gridy = 2;
+		tpr.gridx = 7;
+		topPanelRings.add(reflexesField, tpr);
+
+		tpr.gridy = 3;
+		tpr.gridx = 6;
+		topPanelRings.add(awarenessLabel, tpr);
+
+		tpr.gridy = 3;
+		tpr.gridx = 7;
+		topPanelRings.add(awarenessField, tpr);
+
+		// void ring
+		tpr.insets = new Insets(10, 0, 0, 0);
+		tpr.gridy = 0;
+		tpr.gridx = 8;
+		tpr.gridwidth = 2;
+		topPanelRings.add(voidRingJPEG, tpr);
+		tpr.insets = new Insets(3, 0, 0, 0);
+		tpr.gridwidth = 1;
+
+		JLabel voidRingLabel = new JLabel("Void: ");
+
+		JTextField voidRingField = new JTextField(3);
+		JLabel pointsSpentLabel = new JLabel("Points Spent: ");
+		JTextField pointsSpentField = new JTextField(3);
+
+		tpr.gridwidth = 1;
+		tpr.gridy = 1;
+		tpr.gridx = 8;
+		topPanelRings.add(voidRingLabel, tpr);
+
+		tpr.gridy = 1;
+		tpr.gridx = 9;
+		topPanelRings.add(voidRingField, tpr);
+
+		tpr.gridy = 2;
+		tpr.gridx = 8;
+		topPanelRings.add(pointsSpentLabel, tpr);
+
+		tpr.gridy = 2;
+		tpr.gridx = 9;
+		topPanelRings.add(pointsSpentField, tpr);
+
+		// addings panels to topPanel
 		topPanel.add(BorderLayout.NORTH, topPanelInfo);
 		topPanel.add(BorderLayout.CENTER, topPanelRings);
 
@@ -116,11 +336,21 @@ public class Sheet {
 
 	}
 
+	// action listeners//
+	class SaveMenuItemListener implements ActionListener {
+
+		public void actionPerformed(ActionEvent arg0) {
+			RPGCharacter character = new RPGCharacter();
+
+		}
+
+	}
+
 	public void createNShowGUI() {
 
 		mainWindow = new JFrame("Character Sheet");
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainWindow.setSize(200, 200);
+		// mainWindow.setSize(200, 200);
 
 		// Set up the content pane.
 		addMainWindowItems(mainWindow.getContentPane());
