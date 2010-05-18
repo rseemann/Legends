e21b92ab66189765177495bbc4b28a8e1064997d
Rafael <rseemann@gmail.com>
2010-05-18 18:55:44 -0300

    test

--- src/Testes/RPGCharacterTest.java
+++ src/Testes/RPGCharacterTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Testes;
 
 import static org.junit.Assert.*;
@@ -55,61 +54,4 @@ public class RPGCharacterTest {
 	
 	
 }
-=======
-package Testes;
 
-import static org.junit.Assert.*;
-
-import org.junit.*;
-
-import Ficha.RPGCharacter;
-
-public class RPGCharacterTest {
-	RPGCharacter rpgChar = new RPGCharacter();
-	
-	@Test
-	public void testEarthRingWhenWillPowerIsHigher() {
-		rpgChar.setStamina(10);
-		rpgChar.setWillpower(20);
-		rpgChar.calcRings();
-		assertEquals(10, rpgChar.getEarthRing());
-	}
-
-	@Test
-	public void testEarthRingWhenStaminaIsHigher() {
-		rpgChar.setStamina(20);
-		rpgChar.setWillpower(10);
-		rpgChar.calcRings();
-		assertEquals(10, rpgChar.getEarthRing());
-	}
-	
-	@Test
-	public void testAddSkill(){
-		rpgChar.addSkill("Investigation");
-		assertEquals(1, rpgChar.getSkillRank("investiGAtion"));
-	}
-	
-	@Test
-	public void testAddAnotherSkill(){
-		rpgChar.addSkill("Kenjutsu");
-		assertEquals(1, rpgChar.getSkillRank("kenjutsu"));
-	}
-	
-	@Test
-	public void testKnownSkillsAndRanks(){
-		rpgChar.addSkill("Investigation");
-		rpgChar.addSkill("Kenjutsu");
-		rpgChar.setSkillRank("kenJUtsu", 5);
-		System.out.println(rpgChar.getSkills());
-	}
-	
-	@Test
-	public void testVoidRing() {
-		rpgChar.setVoidTrait(20);
-		rpgChar.calcRings();
-		assertEquals(20, rpgChar.getVoidRing());
-	}
-	
-	
-}
->>>>>>> legends/testing
